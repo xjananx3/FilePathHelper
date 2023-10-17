8736e8a0002b39f1409831b3a5ace930f5f4a3b5
Jan <janan9980@gmail.com>
2023-10-18 00:58:47 +0200

    Iteration 3 - Teile Pfad in Einzeilteile, Ersetze ".."

--- MakePathAbsoluteAgain/FilePathHelper.cs
+++ MakePathAbsoluteAgain/FilePathHelper.cs
@@ -10,6 +10,7 @@ public class FilePathHelper
         }
         
         var nutzerVerzeichnis = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        var projektVerzeichnis = Path.Combine(nutzerVerzeichnis, "RiderProjects/MakePathAbsoluteAgain/MakePathAbsoluteAgain/");
         
         if (filePath.StartsWith("~"))
         {
@@ -21,15 +22,35 @@ public class FilePathHelper
 
         if (filePath.StartsWith("."))
         {
-            var projektVerzeichnis = Path.Combine(nutzerVerzeichnis, "RiderProjects/MakePathAbsoluteAgain/MakePathAbsoluteAgain/");
-            
-            
             var absoluterPfad = filePath.Replace("./", projektVerzeichnis);
             
 
             return absoluterPfad;
         }
 
+        if (filePath.Contains(".."))
+        {
+            string[] pfadTeile = filePath.Split(Path.DirectorySeparatorChar);
+            var bereinigtePfadTeile = new List<string>();
+
+            foreach (var part in pfadTeile)
+            {
+                if (part == ".." && bereinigtePfadTeile.Count > 0)
+                {
+                    bereinigtePfadTeile.RemoveAt(bereinigtePfadTeile.Count - 1);
+                }
+                else if (part != "." && part != "..")
+                {
+                    bereinigtePfadTeile.Add(part);
+                }
+            }
+            
+            string absoluterPfad = string.Join(Path.DirectorySeparatorChar, bereinigtePfadTeile);
+
+
+            return absoluterPfad;
+        }
+        
         return filePath;
 
     }
--- MakePathAbsoluteAgain/Program.cs
+++ MakePathAbsoluteAgain/Program.cs
@@ -4,7 +4,8 @@ using MakePathAbsoluteAgain;
 
 var pfad1 = FilePathHelper.MakeAbsolute("~/Downloads/mountains.jpg");
 var pfad2 = FilePathHelper.MakeAbsolute("./bin/debug/samples/config.json");
-
+var pfad3 = FilePathHelper.MakeAbsolute("/home/jack/RiderProjects/MakePathAbsoluteAgain/MakePathAbsoluteAgain/bin/../program.cs");
 
 Console.WriteLine(pfad1);
 Console.WriteLine(pfad2);
+Console.WriteLine(pfad3);
