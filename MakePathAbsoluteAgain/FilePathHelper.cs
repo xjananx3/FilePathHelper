a729c2e908b78fd9f7ac57351ea42b74e8bd2c5f
Jan <janan9980@gmail.com>
2023-10-17 22:37:19 +0200

    Überprüfung Leerer String hinzugefügt

--- MakePathAbsoluteAgain/FilePathHelper.cs
+++ MakePathAbsoluteAgain/FilePathHelper.cs
@@ -4,14 +4,16 @@ public class FilePathHelper
 {
     public static string MakeAbsolute(string filePath)
     {
+        if (string.IsNullOrWhiteSpace(filePath))
+        {
+            return filePath;
+        }
         
         var nutzerVerzeichnis = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-
-
+        
         if (filePath.StartsWith("~"))
         {
             var absoluterPfad = filePath.Replace("~", nutzerVerzeichnis);
-
             
             return absoluterPfad;
 
